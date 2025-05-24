15ecda79827d9ec98a4780d2647198fcda0c5004
miamicreme <miamicreme@gmail.com>
2025-05-24 02:24:27 -0400

    Remove config

--- config/default.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  scraper: {
-    base: 'http://api.scraperapi.com',
-    opts: { country_code: 'us', device_type: 'desktop' }
-  },
-  queue: { concurrency: 5 },
-  logging: { level: 'info' }
-};
